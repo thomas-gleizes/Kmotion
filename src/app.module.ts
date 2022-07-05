3eaeba7babf1a9c3fabbeef68f82ff3b8ab1618d
Thomas <thomasgleizes34@gmail.com>
2022-07-05 23:07:03 +0200

    init gateway module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { UserModule } from './user/user.module';
     UserModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [AppGateway],
 })
 export class AppModule {}
