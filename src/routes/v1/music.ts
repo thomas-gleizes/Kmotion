080930f00cdf5b09b43971488561984f8b11effc
Thomas <thomasgleizes34@gmail.com>
2022-12-15 20:44:10 +0100

    continue

--- src/routes/v1/music.ts
+++ src/routes/v1/music.ts
@@ -1,10 +1,32 @@
 import { FastifyInstance } from "fastify"
 import isLogin from "middlewares/isLogin"
+import prisma from "utils/prisma"
+import BadRequestException from "exceptions/http/BadRequestException"
+import isAdmin from "middlewares/isAdmin"
+import YtConverter from "services/ytconverter"
 
 export default function musicRoutes(instance: FastifyInstance, options: any, done: Function) {
   instance.addHook("onRequest", isLogin)
 
-  instance.post<{ Params: { id: string } }>("/add", async (request, reply) => {})
+  const ytConverter = YtConverter.getInstance()
+
+  instance.get("/sync", { onRequest: [isAdmin] }, async (request, reply) => {
+    const musics = await ytConverter.musics()
+
+    for (const music of musics) {
+      const find = await prisma.music.findUnique({ where: { youtubeId: music.id } })
+    }
+
+    return { success: true, meta: { new: 5 } }
+  })
+
+  instance.post<{ Params: { id: string } }>("/:id/add", async (request, reply) => {
+    const music = await prisma.music.findUnique({ where: { id: +request.params.id } })
+
+    if (music) throw new BadRequestException("Music already exists")
+
+    reply.send({ success: "true" })
+  })
 
   done()
 }
--- src/services/ytconverter.ts
+++ src/services/ytconverter.ts
@@ -1,6 +1,36 @@
 const token = process.env.CONVERTER_TOKEN as string
-const url = process.env.CONVERTER_URL as string
 
-export const dlAudio = (id: string) => {
-  return fetch(`${url}/static/${id}/${id}.mp3`, { headers: { authorization: token } })
+export default class YtConverter {
+  private static instance: YtConverter
+
+  private _url: string
+  private _token: string
+
+  private constructor() {
+    this._url = process.env.CONVERTER_URL as string
+    this._token = process.env.CONVERTER_TOKEN as string
+  }
+
+  public static getInstance(): YtConverter {
+    if (!YtConverter.instance) {
+      YtConverter.instance = new YtConverter()
+    }
+
+    return YtConverter.instance
+  }
+
+  private getHeaders(): Headers {
+    const headers = new Headers()
+    headers.append("authorization", this._token)
+
+    return headers
+  }
+
+  public async musics(): Promise<any> {
+    return fetch(this._url + "/musics", { method: "GET", headers: this.getHeaders() }).then(
+      (response) => response.json()
+    )
+  }
+
+  public async info(url: string): Promise<any> {}
 }
