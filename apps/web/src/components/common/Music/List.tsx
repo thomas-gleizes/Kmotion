7ef68f251b851216912d13daaf134d471221aee5
Thomas <thomasgleizes34@gmail.com>
2023-04-22 12:28:22 +0200

    feat(web): remove sleep bettwen dialog.open and add confirmDialog on delete by admin a music

--- apps/web/src/components/common/Music/List.tsx
+++ apps/web/src/components/common/Music/List.tsx
@@ -124,8 +124,7 @@ export const MusicItemActions: Component<MusicItemActionsProps> = ({ music, acti
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items
-            className="z-[100000000000] absolute right-0 mt-2 w-56 p-1 origin-top-right divide-y divide-gray-100 rounded-lg bg-secondary/90 backdrop-blur-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="z-[100000000000] absolute right-0 mt-2 w-56 p-1 origin-top-right divide-y divide-gray-100 rounded-lg bg-secondary/90 backdrop-blur-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {actions.map((group, index) =>
               group.length ? (
                 <div key={index} className="py-1">
