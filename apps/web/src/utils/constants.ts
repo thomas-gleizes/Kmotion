280756922fb4f5ef9ca05e45a0801a11d45d711b
thomas <thomasgleizes34@gmail.com>
2023-05-12 11:45:13 +0200

    feat(web): restart music on same where is paused last time when page was close

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -1,13 +1,15 @@
 export const WINDOW_MESSAGE = {
   logout: "logout",
-}
+} as const
 
 export const QUERIES_KEY = {
   playlists: ["playlists"],
   musics_search: ["musics", "search"],
-}
+} as const
 
 export const LOCAL_STORAGE_KEYS = {
   AUTH_TOKEN: "auth_token",
   AUTH: "authenticated",
-}
+  TIME: "time",
+  VOLUME: "volume",
+} as const
