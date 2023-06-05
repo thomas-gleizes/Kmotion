5120e3cf3cd9fb28a5725a5a37fa598acddcf2c4
thomas <thomasgleizes34@gmail.com>
2023-06-05 10:48:06 +0200

    feat(web): add list action on current music played

--- apps/web/src/components/common/Player/FullscreenPlayer.tsx
+++ apps/web/src/components/common/Player/FullscreenPlayer.tsx
@@ -1,17 +1,21 @@
-import React, { useRef } from "react"
+import React, { useMemo, useRef } from "react"
 import classnames from "classnames"
 import { useToggle } from "react-use"
 import {
+  CgRowFirst,
   FaBackward,
   FaBars,
   FaEllipsisH,
   FaForward,
+  FaList,
   FaListUl,
   FaPause,
   FaPlay,
   FaRandom,
   FaSync,
   FaSyncAlt,
+  FaTimes,
+  FaTrash,
   FaVolumeDown,
   FaVolumeUp,
 } from "react-icons/all"
@@ -24,6 +28,14 @@ import Slider from "../Slider"
 import QueueList from "./QueueList"
 import { useLayoutContext } from "../../../contexts/layout"
 import ScrollableContainer from "../../layouts/ScrollableContainer"
+import { MusicItemActions } from "../Music/List"
+import { IMusic } from "@kmotion/types"
+import { api } from "../../../utils/Api"
+import { useDialog } from "react-dialog-promise"
+import AddToPlaylist from "../../modals/AddToPlaylist"
+import EditPlaylist from "../../modals/EditPlaylist"
+import ConfirmDialog from "../../modals/ConfirmDialog"
+import { useAuthenticatedContext } from "../../../contexts/auth"
 
 interface Props {
   isOpen: boolean
@@ -46,6 +58,7 @@ interface Props {
 const FullscreenPlayer: Component<Props> = ({ isOpen, state, controls }) => {
   const { currentMusic, playlist, actions, loop, assets } = usePlayerContext()
   const { isLaggedBlur } = useLayoutContext()
+  const { user } = useAuthenticatedContext()
 
   const tRef = useRef<HTMLHeadingElement>(null)
 
@@ -53,6 +66,69 @@ const FullscreenPlayer: Component<Props> = ({ isOpen, state, controls }) => {
 
   const isOverflow =
     (tRef.current?.offsetWidth || 0) >= (tRef.current?.parentElement?.offsetWidth || 2000)
+
+  const addToPlaylist = useDialog(AddToPlaylist)
+  const editPlaylist = useDialog(EditPlaylist)
+  const confirmDialog = useDialog(ConfirmDialog)
+
+  const handleAddToPlaylist = async (music: IMusic) => {
+    const result = await addToPlaylist.open({ music })
+
+    if (result === "create-playlist") {
+      await new Promise((resolve) => setTimeout(resolve, 500))
+
+      const result = await editPlaylist.open({
+        isNew: true,
+        musics: [],
+        initialValues: { title: "", description: "", musics: [] },
+      })
+
+      await new Promise((resolve) => setTimeout(resolve, 500))
+      if (result.action === "success-new") {
+        await handleAddToPlaylist(music)
+      }
+    }
+  }
+
+  const listActions = useMemo(() => {
+    const listActions = [
+      [
+        {
+          label: "Ajouter à une playlist",
+          icon: <FaList />,
+          className: "hover:bg-white/30",
+          onClick: handleAddToPlaylist,
+        },
+      ],
+    ]
+
+    if (user.isAdmin) {
+      listActions[0].unshift({
+        label: "Supprimer",
+        icon: <FaTrash />,
+        className: "text-primary hover:bg-primary/30",
+        onClick: (music: IMusic) =>
+          confirmDialog
+            .open({
+              message: (
+                <span>
+                  Voulez vous supprimer la music "{music.title}" de {music.artist} <br /> Cette
+                  action est définitive
+                </span>
+              ),
+            })
+            .then((result) => {
+              if (result)
+                api
+                  .deleteMusic(music.id)
+                  .then(() => actions.remove(actions.findIndex(music)))
+                  .catch((err) => console.log("delete failed", err))
+            }),
+      })
+    }
+
+    return listActions
+  }, [user])
 
   if (!currentMusic) return null
 
@@ -101,10 +177,8 @@ const FullscreenPlayer: Component<Props> = ({ isOpen, state, controls }) => {
           {currentMusic.artist}
         </p>
       </div>
-      <div className="flex justify-end items-center pl-3">
-        <div className="cursor-pointer bg-white/40 rounded-full p-2">
-          <FaEllipsisH className="text-white text-lg lg:text-2xl" />
-        </div>
+      <div>
+        <MusicItemActions actions={listActions} music={currentMusic} />
       </div>
     </div>
   )
