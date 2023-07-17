04df2893f8f506d72721f04aa1f56eba4f999871
thomas <thomasgleizes34@gmail.com>
2023-07-17 14:38:14 +0200

    fix(web): try to fix media session on ios

--- apps/web/src/utils/helpers.ts
+++ apps/web/src/utils/helpers.ts
@@ -20,3 +20,55 @@ export function handleStopPropagation(callback: (event: MouseEvent) => void): Ev
 export function isMobileOrTablet(): boolean {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 }
+
+export function isIos(): boolean {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent)
+}
+
+export function resizeImage(src: string, width: number, height: number): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+
+    console.log("Img", img)
+
+    img.onload = function () {
+      console.log("Onload")
+      const canvas = document.createElement("canvas")
+      const context = canvas.getContext("2d")
+
+      if (!context) return reject("Context is null")
+
+      const ratio = Math.min(width / img.width, height / img.height)
+
+      canvas.width = img.width * ratio
+      canvas.height = img.height * ratio
+
+      context.drawImage(img, 0, 0, canvas.width, canvas.height)
+
+      canvas.toBlob(
+        (blob) => {
+          console.log("Blob", blob)
+
+          if (blob) resolve(blob)
+          else reject(new Error("Blob is null"))
+        },
+        "image/jpeg",
+        0.95,
+      )
+    }
+
+    img.src = src
+  })
+}
+
+export function getImageResolution(src: string): Promise<{ width: number; height: number }> {
+  return new Promise<{ width: number; height: number }>((resolve) => {
+    const img = new Image()
+
+    img.onload = function () {
+      resolve({ width: img.width, height: img.height })
+    }
+
+    img.src = src
+  })
+}
