42a4a402b87d94328bd6cc208a903698e8593905
Thomas <thomasgleizes34@gmail.com>
2023-11-01 10:00:27 +0100

    fix(api): change plainToClass (deprecated) to plainToInstance

--- apps/api/src/decorators/validations.ts
+++ apps/api/src/decorators/validations.ts
@@ -1,5 +1,5 @@
 import { FastifyRequest } from "fastify"
-import { ClassConstructor, plainToClass } from "class-transformer"
+import { ClassConstructor, plainToInstance } from "class-transformer"
 import { validate } from "class-validator"
 
 import BadRequestException from "../exceptions/http/BadRequestException"
@@ -7,7 +7,7 @@ import { Dto } from "@kmotion/validations"
 
 export function validateBody<S extends Dto>(dtoClass: ClassConstructor<S>) {
   return async (request: FastifyRequest<{ Body: S }>) => {
-    const body = plainToClass(dtoClass, request.body)
+    const body = plainToInstance(dtoClass, request.body)
     const errors = await validate(body)
 
     if (errors.length > 0) throw new BadRequestException("A body validation error occurred", errors)
@@ -18,7 +18,7 @@ export function validateBody<S extends Dto>(dtoClass: ClassConstructor<S>) {
 
 export function validateParams<S extends Dto>(dtoClass: ClassConstructor<S>) {
   return async (request: FastifyRequest<{ Params: S }>) => {
-    const params = plainToClass(dtoClass, request.params)
+    const params = plainToInstance(dtoClass, request.params)
     const errors = await validate(params)
 
     if (errors.length > 0)
@@ -30,7 +30,7 @@ export function validateParams<S extends Dto>(dtoClass: ClassConstructor<S>) {
 
 export function validateQuery<S extends Dto>(dtoClass: ClassConstructor<S>) {
   return async (request: FastifyRequest) => {
-    const query = plainToClass(dtoClass, request.query)
+    const query = plainToInstance(dtoClass, request.query)
     const errors = await validate(query)
 
     if (errors.length > 0)
