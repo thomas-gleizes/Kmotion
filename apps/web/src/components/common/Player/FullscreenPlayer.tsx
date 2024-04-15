cccce4bee391cb3972e954ca7712f2e177439466
Thomas Gleizes <thomasgleizes34@gmail.coml>
2024-04-15 14:46:07 +0200

    feat(web): disable draggalbe on img of music

--- apps/web/src/components/common/Player/FullscreenPlayer.tsx
+++ apps/web/src/components/common/Player/FullscreenPlayer.tsx
@@ -163,6 +163,7 @@ const FullscreenPlayer: Component<Props> = ({ isOpen, state, controls }) => {
                       src={assets.cover.url}
                       alt={currentMusic.title}
                       onClick={() => (showQueue && isMobile ? toggleShowQueue() : playing.toggle())}
+                      draggable="false"
                       className={classnames(
                         "rounded-lg w-full transform transition-all duration-300 lg:rounded-2xl shadow-2xl select-none",
                         { "scale-[75%] shadow-lg": !playing.value },
